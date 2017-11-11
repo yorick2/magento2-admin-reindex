d9111595c369691406635b3c1bb4cf6c2593eb37
Paul Millband <millband_paul@hotmail.co.uk>
2017-11-11 11:11:42 +0000

    add reindex to list

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * paulmillband_adminReindex Module Registration.
+ *
+ * @category    paulmillband
+ *
+ * @author      paulmillband
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'paulmillband_adminReindex',
+    __DIR__
+);
