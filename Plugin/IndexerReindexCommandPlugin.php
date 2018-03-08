bea152410737af8c6b810b1962812fc943d5ab51
Paul Millband <paul@junowebdesign.com>
2018-03-08 17:59:03 +0000

    clearly warn of hung reindexes

---
+++ Plugin/IndexerReindexCommandPlugin.php
@@ -0,0 +1,73 @@
+<?php
+namespace Paulmillband\Reindex\Plugin;
+
+use Magento\Framework\Indexer\StateInterface;
+use Magento\Indexer\Model\Indexer\CollectionFactory;
+use Magento\Indexer\Model\IndexerFactory;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Output\ConsoleOutput;
+
+class IndexerReindexCommandPlugin
+{
+    /**
+     * @var IndexerFactory
+     */
+    protected $indexerFactory;
+
+    /**
+     * @var CollectionFactory
+     */
+    protected $indexerCollectionFactory;
+
+    /**
+     * IndexerReindexCommandPlugin constructor.
+     * @param IndexerFactory $indexerFactory
+     * @param CollectionFactory $indexerCollectionFactory
+     */
+    public function __construct(
+        IndexerFactory $indexerFactory,
+        CollectionFactory $indexerCollectionFactory
+    ) {
+        $this->indexerFactory = $indexerFactory;
+        $this->indexerCollectionFactory = $indexerCollectionFactory;
+    }
+
+    /**
+     * echos a warning if magento has an index described as working after the reindex has finished. This is likely to be
+     * a failed process
+     *
+     * @param Command $subject
+     * @param callable $proceed
+     * @param ArgvInput $input
+     * @param ConsoleOutput $output
+     * @return mixed
+     */
+    public function aroundRun(Command $subject, callable $proceed, ArgvInput $input, ConsoleOutput $output)
+    {
+        $returnValue = $proceed($input, $output); // run original code
+        $workingIndexers = [];
+
+        $indexers = $this->indexerCollectionFactory->create();
+        if ($input->getArgument('command') !== "indexer:reindex") {
+            return $returnValue;
+        }
+        foreach ($indexers as $indexer) {
+            if ($indexer->getStatus() == StateInterface::STATUS_WORKING) {
+                $workingIndexers[] = $indexer->getTitle();
+            }
+        }
+        $var = implode(', ', $workingIndexers);
+        echo <<<EOT
+
+==== WARNING: Not all indexes were run ====
+Indexes marked as working found. These indexes may still be running with another process or have had a fatal
+error before (e.g. time-out error). If this issue persists update the index_state table in your magento database.
+For the relevant row set the status to valid or failed and try again.
+
+Indexes not indexed: $var
+
+EOT;
+        return $returnValue;
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # magento 2 manual reindex module
 Adds a reindex option to the reindex page in the magento admin.
 
+This module also gives a clear warning if a command line reindex process skips an index table. The standard warning can
+be missed. These are caused by indexes that may still be running with another process or have had a fatal error before
+(e.g. time-out error) not allowing the flag to be updated. This stops this reindex process from being run, unless the
+database in updated manually.
+
 ## Installation
 - composer config repositories.paulmillband-m2-admin-reindex vcs git@github.com:yorick2/magento2-admin-reindex.git
 - composer require paulmillband/m2-admin-reindex:dev-master
